2e7ef40f6d7a84627a06333e1f8ec90ba263273b
Luigi Maiorano <lmaiorano97@gmail.com>
2024-02-13 12:29:49 +0100

    moved env activation to post hook script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+ACTIVATE_ENV=$(which activate)
+PROJECT_NAME={{cookiecutter.repo_name}}
 
-# # Run the create_environment Makefile command
-# echo "Creating environment..."
-# make create_environment
+# Run the create_environment Makefile command
+echo "Creating environment..."
+make create_environment
 
-# # initialize git
-# git init
+# activate the environment
+source $ACTIVATE_ENV $PROJECT_NAME
+
+echo ">>> Install requirements using pip"
+pip install -r requirements.txt
+
+# Pip freeze requirements
+echo ">>> Freezing requirements"
+pip freeze > requirements.txt
+
+#initialize git
+echo "Initializing git..."
+git init
