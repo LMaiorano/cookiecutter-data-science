26e96f84e135da811401851d45f2a33aa6dbacba
Luigi Maiorano <lmaiorano97@gmail.com>
2024-02-13 11:46:08 +0100

    deactivate post hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Run the create_environment Makefile command
-echo "Creating environment..."
-make create_environment
+# # Run the create_environment Makefile command
+# echo "Creating environment..."
+# make create_environment
 
+# # initialize git
+# git init
