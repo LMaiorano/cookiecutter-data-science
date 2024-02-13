1d207ab20fee287513181256bf9404d427870a37
Luigi Maiorano <lmaiorano97@gmail.com>
2024-02-13 12:59:36 +0100

    added jupyter kernel install option

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ACTIVATE_ENV=$(which activate)
 PROJECT_NAME={{cookiecutter.repo_name}}
+INSTALL_JUPYTER={{ cookiecutter.use_jupyter_notebooks }} #true or false
 
 # Run the create_environment Makefile command
 echo "Creating environment..."
@@ -11,6 +12,13 @@ source $ACTIVATE_ENV $PROJECT_NAME
 
 echo ">>> Install requirements using pip"
 make reqs
+
+# Install Jupyter Kernel if necessary
+if [ "$INSTALL_JUPYTER" == "true" ]; then
+    echo ">>> Installing Jupyter Kernel"
+    pip install ipykernel  
+    python -m ipykernel install --user --display-name ${PWD} --name ${PWD##*/}
+fi
 
 #initialize git
 echo "Initializing git..."
