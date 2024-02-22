003b4b3b5ce71093951b54fd7ccc1b1b45a71861
Luigi Maiorano <lmaiorano97@gmail.com>
2024-02-22 16:31:35 +0100

    path not working

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,11 @@
-import os
 import platform
 import subprocess
-from pathlib import Path
 
 
-unix_sh_path = str(Path(Path(__file__).parent, 'unix_setup.sh'))
 
 def run_unix_setup():
     if platform.system() in ['Linux', 'Darwin']:  # Darwin is for macOS
-        subprocess.run([unix_sh_path], shell=True)
+        subprocess.run(['unix_setup.sh'], shell=True)
     else:
         print("This system is not Unix-based. Please manually setup your environment or use the provided Makefile.")
 
