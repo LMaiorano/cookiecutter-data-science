a341410d402ad71279fdf43e938a02a17cdc06b2
Luigi Maiorano <lmaiorano97@gmail.com>
2024-03-11 11:53:51 +0100

    added pre-commit hook config

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,13 +1,20 @@
 # Run OS check here, since this is executed in the root directory of a copy of the repository directory
 import os
 import platform
-
+import shutil
 
 
 def select_platform_post_hook():
     if platform.system() in ['Linux', 'Darwin']:  # Darwin is for macOS
         os.rename('hooks/_post_gen_project.sh', 'hooks/post_gen_project.sh')
         # TODO: Check that make command is available
+        if shutil.which('make') is None:
+            print("'make' command is not available")
+            print("Please install 'make' before continuing")
+            
+            # abort the cookiecutter
+            exit(1)
+            
         
     else:
         os.rename('hooks/_post_gen_project.py', 'hooks/post_gen_project.py')
