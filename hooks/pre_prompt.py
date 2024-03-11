7212e2312e05560ef77ff62353234fbe103a2baf
Luigi Maiorano <lmaiorano97@gmail.com>
2024-03-11 14:31:40 +0100

    pre-prompt get python version

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -4,15 +4,15 @@ import platform
 import shutil
 import requests
 
-def latest_python_version():
+def latest_python_versions():
     result = requests.get("https://endoflife.date/api/python.json")
     parsed_result = result.json()
-    reccomended_v = parsed_result[2]["latest"] # two versions back
+    recommended_v = ''.join(parsed_result[2]["latest"].split('.')[:2]) # two versions back
     latest_v =  parsed_result[0]["latest"]
     
-    choices = [reccomended_v, latest_v]
+    choices = [recommended_v, latest_v]
     
-    print(f"Possible Python versions: {choices}")
+    # print(f"Possible Python versions: {choices}")
 
     # print(f"Latest Python version: {version_string}")
     return choices
@@ -36,5 +36,25 @@ def select_platform_post_hook():
     
 select_platform_post_hook()
 
-latest_python_version()
 
+
+def create_cookiecutter_json():
+    
+    version_choices = latest_python_versions()
+    
+    # Read the cookiecutter.json file
+    with open('cookiecutter.json', 'r') as file:
+        data = file.readlines()
+    
+    # find line with python_version
+    pv_idx = [i for i, line in enumerate(data) if "python_version" in line][0]
+    
+    # replace PRE-PROMPT-LATEST with version_choices[0]
+    data[pv_idx] = data[pv_idx].replace("PRE-PROMPT-LATEST", version_choices[0])
+    data[pv_idx] = data[pv_idx].replace("PRE-PROMPT-RECOMMENDED", version_choices[1])
+    
+    # Write the cookiecutter.json file
+    with open('cookiecutter.json', 'w') as file:
+        file.writelines(data)
+
+create_cookiecutter_json()
